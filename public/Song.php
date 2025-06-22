bad3d46a42717f5cbb34bb3383ef6b11acae9d39
Pequeno Maxence <maxence.pequeno@icloud.com>
2025-06-22 21:58:54 +0200

    Création de songCollection.php et ajout de la fonction de redirection vers lien youtube. Création de findbyID dans artist

--- public/Song.php
+++ public/Song.php
@@ -2,18 +2,24 @@
 declare(strict_types=1);
 
 use Entity\album;
+use Entity\artist;
 use Entity\Collection;
 use Entity\song;
 use html\AppWebPage;
 
 $songId = (int)$_GET['songId'];
 $song = song::findById($songId);
+$songName = $song->getName();
 
 $track = song::findTrackBySongId($songId);
 $SongDuractionInminutes = intdiv($track->getDuration(), 60);
 $SongDuractionInSeconde = $track->getDuration() % 60;
 
 $album = Album::findById($track->getAlbumId());
+$artist  = Artist::findById($album->getArtistId());
+$artistName = $artist->getName();
+
+$link = Collection\songCollection::YoutubeLinkWithArtistAndSongName($artistName, $songName);
 
 $webpage = new AppWebPage();
 $webpage->setTitle($song->getName());
@@ -28,6 +34,11 @@ $webpage->appendContent(<<<HTML
     <div class="stats">
         <h3>Numéro dans l'album : {$track->getNumber()}</h3>
         <h3>Durée du titre : {$SongDuractionInminutes} mins, {$SongDuractionInSeconde} sec</h3>
+        
+        <a href="{$link}"> 
+        <h3>Lien Youtube</h3>
+        </a>
+    
     </div>
 </div>
 HTML);
---
+++ src/Entity/Collection/songCollection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Entity\Collection;
+
+class songCollection
+{
+    public static function YoutubeLinkWithArtistAndSongName(string $songName, string $artistName): string{
+        $baselink = 'https://www.youtube.com/results?search_query=';
+        $baselink .= "{$artistName}+{$songName}";
+        return $baselink;
+
+    }
+}
